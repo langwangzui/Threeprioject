95322c16787034f110e7b0757c7dcaa1ba285136
Tody_liu <574294003@qq.com>
2018-06-25 16:35:50 +0800

    add liu1

---
+++ src/main/java/com/service/BizService.java
@@ -0,0 +1,6 @@
+package com.service;
+
+import com.dao.InfoMapper;
+
+public interface BizService extends InfoMapper {
+}
---
+++ src/main/java/com/test/Demo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.dao.InfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Demo {
+
+    @Autowired
+    private InfoMapper infoMapper;
+
+
+
+
+}
