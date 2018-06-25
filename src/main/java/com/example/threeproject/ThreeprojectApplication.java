cb9ff66df92bbedcc2f5d16f08ef5e9320bcd4e9
langwangzui <2915419165@qq.com>
2018-06-25 13:11:52 +0800

    add

---
+++ src/main/java/com/example/threeproject/ThreeprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.threeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/threeproject/ThreeprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.threeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreeprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
